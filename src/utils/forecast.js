df8e3fd723eda9104ca9b4664ea9f55e294bb4d9
aashir choudhry <a.choudhry@productivetechnologies.co.in>
2020-07-28 23:25:27 +0530

    just checking weather api

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const forecast = (lat, long, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=340823cbf780dce6954aa9826330396b&query='+lat+','+long+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=340823cbf780dce6954aa9826330396b&query='+lat+','+long
 
     request({url, json: true}, (error, response) => {
         if(error){
@@ -10,7 +10,8 @@ const forecast = (lat, long, callback) => {
         } else if (response.body.error) {
             callback('no such stuff bruh')
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]} it is currently ${response.body.current.temperature} degrees out but it feels like ${response.body.current.humidity} humid` )
+            //console.log(response.body)
+            callback(undefined, `${response.body.current.weather_descriptions[0]} it is currently ${response.body.current.observation_time} o clock but it feels like ${response.body.current.precip} % chance of rain`)
         }
     })
 
